7608a3aa7d18d5c42e5adbda709e258fae1520f7
chrisbeltignino <assasin.tignino@gmail.com>
2022-09-22 23:34:33 -0300

    Agregar archivos de proyecto.

---
+++ Usuarios/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Usuarios
+{
+    public class Usuario
+    {
+        private string nombreUsuario;
+        private string contraseña;
+
+        public Usuario(string nomUsua, string contra)
+        {
+            this.nombreUsuario = nomUsua;
+            this.contraseña = contra;
+        }
+
+    }
+}
