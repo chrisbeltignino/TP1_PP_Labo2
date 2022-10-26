7b8724431cb9ace9bff264d28b4ca7e045dd2bf6
chrisbeltignino <assasin.tignino@gmail.com>
2022-10-26 09:59:50 -0300

    actualizo

--- FormMain/Program.cs
+++ FormMain/Program.cs
@@ -17,7 +17,7 @@ namespace FormMain
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
+            Application.Run(new FormLogin());
         }
     }
 }
